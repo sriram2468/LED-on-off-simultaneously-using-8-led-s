3a422380c0793222184cfe291900f870bd15f12a
sriram2468 <160378452+sriram2468@users.noreply.github.com>
2024-08-06 18:59:58 +0530

    Add files via upload

---
+++ LED_ON.ino
@@ -0,0 +1,72 @@
+void setup()
+{
+  pinMode(D0, OUTPUT);
+  pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
+  pinMode(D5, OUTPUT);
+  pinMode(D6, OUTPUT);
+  pinMode(D8, OUTPUT);
+
+  
+}
+
+void loop() 
+{
+ digitalWrite(D0, HIGH);
+ delay(25);
+ digitalWrite(D0, 0);
+ digitalWrite(D1, HIGH);
+ delay(25);
+ digitalWrite(D1, 0);
+ digitalWrite(D2, HIGH);
+ delay(25);
+  digitalWrite(D2, 0);
+ digitalWrite(D5, HIGH);
+ delay(25);
+  digitalWrite(D5, 0);
+ digitalWrite(D6, HIGH);
+ delay(25);
+  digitalWrite(D6, 0);
+ digitalWrite(D7, HIGH);
+ delay(25);
+  digitalWrite(D7, 0);
+ digitalWrite(D8, HIGH);
+ delay(25);
+  digitalWrite(D8, 0);
+ digitalWrite(D3, HIGH);
+ delay(25);
+  digitalWrite(D3, 0);
+ digitalWrite(D3, HIGH);
+ delay(25);
+  digitalWrite(D3, 0);
+ digitalWrite(D8, HIGH);
+ delay(25);
+  digitalWrite(D8, 0);
+ digitalWrite(D7, HIGH);
+ delay(25);
+  digitalWrite(D7, 0);
+ digitalWrite(D6, HIGH);
+ delay(25);
+  digitalWrite(D6, 0);
+ digitalWrite(D5, HIGH);
+ delay(25);
+  digitalWrite(D5, 0);
+ digitalWrite(D2, HIGH);
+ delay(25);
+  digitalWrite(D2, 0);
+ digitalWrite(D1, HIGH);
+ delay(25);
+  digitalWrite(D1, 0);
+ digitalWrite(D0, HIGH);
+ delay(25);
+  digitalWrite(D0, 0);
+ digitalWrite(D0, HIGH);
+ delay(25);
+ 
+
+ 
+ 
+
+}
